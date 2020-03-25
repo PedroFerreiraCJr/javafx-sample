28ee38ef36a4f38701d73c4f8e1e1ffc20abbc2b
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-03-25 01:18:44 -0300

    remember me func complete

--- src/main/java/br/com/dotofcodex/javafx_sample/samples/Sample2.java
+++ src/main/java/br/com/dotofcodex/javafx_sample/samples/Sample2.java
@@ -1,6 +1,11 @@
 package br.com.dotofcodex.javafx_sample.samples;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Optional;
+import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,10 +37,18 @@ import javafx.stage.WindowEvent;
 public class Sample2 extends Application {
 
 	private static final Logger logger = LoggerFactory.getLogger(Sample2.class);
+	private static final String PROPERTIES_FILE_LOCATION = "/home/pedro/.javafx_sample.properties";
+
+	private String lastUsername;
 
 	public Sample2() {
 		super();
 		logger.info("Sample 2 class instance initialized");
+	}
+
+	@Override
+	public void init() {
+		loadUsername();
 	}
 
 	@Override
@@ -73,6 +86,7 @@ public class Sample2 extends Application {
 				}
 			}
 		});
+		username.setText(lastUsername);
 		grid.add(username, 1, 1);
 
 		Label label2 = new Label("Password: ");
@@ -96,8 +110,21 @@ public class Sample2 extends Application {
 		HBox rememberBox = new HBox(10);
 		rememberBox.setAlignment(Pos.BOTTOM_LEFT);
 		rememberBox.getChildren().add(rememberMe);
-		grid.add(rememberBox, 1, 3);
 		
+		if (lastUsername != null) {
+			rememberMe.setSelected(true);
+		}
+		
+		rememberMe.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if (!newValue) {
+					clearUsername();
+				}
+			}
+		});
+		grid.add(rememberBox, 1, 3);
+
 		Button login = new Button("Sign in");
 		login.setDefaultButton(true);
 		login.setOnAction((ActionEvent event) -> {
@@ -125,8 +152,11 @@ public class Sample2 extends Application {
 				String message = "Logged in with success.";
 				if (rememberMe.isSelected()) {
 					message = message.concat("\nRemember me active.");
+					saveUsername(user);
+				} else {
+					clearUsername();
 				}
-				
+
 				Alert alert = new Alert(AlertType.INFORMATION);
 				alert.setTitle("Info Dialog");
 				alert.setHeaderText("Hello, Pedro");
@@ -170,6 +200,44 @@ public class Sample2 extends Application {
 
 		stage.show();
 
+	}
+
+	private void saveUsername(String username) {
+		Properties properties = new Properties();
+		properties.put("username", username);
+		try {
+			properties.store(new FileOutputStream(PROPERTIES_FILE_LOCATION), null);
+			logger.info("properties file saved with success.");
+		} catch (IOException e) {
+			logger.error("failed to save properties file");
+			e.printStackTrace();
+		}
+	}
+
+	private void loadUsername() {
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream(PROPERTIES_FILE_LOCATION));
+			lastUsername = (String) props.get("username");
+			logger.info("properties file loaded with success");
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (e instanceof FileNotFoundException) {
+				logger.info("failed to load the file - not found");
+			}
+		}
+	}
+
+	private void clearUsername() {
+		Properties properties = new Properties();
+		properties.clear();
+		try {
+			properties.store(new FileOutputStream(PROPERTIES_FILE_LOCATION), null);
+			logger.info("properties file cleared and saved with success.");
+		} catch (IOException e) {
+			logger.error("failed to save properties file");
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
